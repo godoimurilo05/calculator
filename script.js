121e53e8694ea2d3abdac3d1adab27a0815c8d27
Murilo Godoi <godoi.murilo08@gmail.com>
2024-07-11 15:13:57 -0300

    Add function and Clear function added

---
+++ script.js
@@ -0,0 +1,36 @@
+//resgatar os botões
+const buttons = document.querySelectorAll('.btn_calculator');
+let elements = new Array;
+//mostrar o valor do botão no console 
+buttons.forEach(e => {
+   e.addEventListener('click', () => {   
+    calculatorButton = e.value;
+    if (calculatorButton >= 0 || calculatorButton === ".") 
+    {
+        addElement(calculatorButton)
+        
+
+    }
+    if(calculatorButton === 'C')
+    {
+        clearCalculatorDisplay()
+    }
+    
+})
+});
+
+function addElement(num) {
+    const displayScreen = document.querySelector('.calculator__results__screen');
+    let displayValue = displayScreen.value;
+    // Certifique-se de que displayValue seja uma string
+    displayValue = String(displayValue);
+    // Concatena o número ao valor atual da tela e atualiza o valor na tela
+    displayScreen.value = displayValue + num;
+    // Exibe o valor atualizado no console
+
+}
+
+function clearCalculatorDisplay() {
+    document.querySelector('.calculator__results__screen').value = ''
+    
+}
