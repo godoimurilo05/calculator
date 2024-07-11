a90f42c23d67ab4a623ed5d15b605ee33ff17ba8
Murilo Godoi <godoi.murilo08@gmail.com>
2024-07-11 15:54:35 -0300

    Calculator finished

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
 //resgatar os botões
 const buttons = document.querySelectorAll('.btn_calculator');
-let elements = new Array;
 //mostrar o valor do botão no console 
 buttons.forEach(e => {
    e.addEventListener('click', () => {   
     calculatorButton = e.value;
-    if (calculatorButton >= 0 || calculatorButton === ".") 
-    {
-        addElement(calculatorButton)
-        
-
-    }
     if(calculatorButton === 'C')
     {
         clearCalculatorDisplay()
     }
-    
+    if (calculatorButton === '=') {
+        calculate()
+    }
+    if (calculatorButton === '<') {
+        deleteLastElement()
+    }
+    if (calculatorButton !== 'C'  && calculatorButton !== '=' && calculatorButton !== '<') 
+    {
+        addElement(calculatorButton)
+    }
 })
 });
 
@@ -27,10 +29,18 @@ function addElement(num) {
     // Concatena o número ao valor atual da tela e atualiza o valor na tela
     displayScreen.value = displayValue + num;
     // Exibe o valor atualizado no console
-
 }
 
 function clearCalculatorDisplay() {
     document.querySelector('.calculator__results__screen').value = ''
-    
+}
+
+function calculate() {
+    const displayValue = document.querySelector('.calculator__results__screen').value;
+    document.querySelector('.calculator__results__screen').value = eval(displayValue)
+}
+
+function deleteLastElement() {
+    const displayValue = document.querySelector('.calculator__results__screen').value;
+    document.querySelector('.calculator__results__screen').value = displayValue.slice(0, -1);
 }
